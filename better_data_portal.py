d854afb8de4390a0daf0c83db93359089aedc89f
Anthony Moser <anthony.moser@gmail.com>
2023-01-27 21:37:36 -0600

    page config is no longer a beta feature

--- better_data_portal.py
+++ better_data_portal.py
@@ -172,7 +172,7 @@ try:
 except Exception:
     app_token = None
 
-st.beta_set_page_config(
+st.set_page_config(
     layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
     initial_sidebar_state="expanded",  # Can be "auto", "expanded", "collapsed"
     page_title='Better Data Portal',  # String or None. Strings get appended with "• Streamlit".
